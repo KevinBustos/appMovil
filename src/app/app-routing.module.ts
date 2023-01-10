02e32188856694424d62c29c38bea64806ae2fbd
Kevin Bustos <kevinbustossoto@gmail.com>
2023-01-10 14:16:09 -0300

    se creo la pagina del clima

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'clima',
+    loadChildren: () => import('./clima/clima.module').then( m => m.ClimaPageModule)
   }
 ];
 
