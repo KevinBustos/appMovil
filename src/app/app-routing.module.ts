5e6d3075ec7778cd2e5e371c1f259f6dfe857b4c
Kevin Bustos <kevinbustossoto@gmail.com>
2023-01-12 20:33:41 -0300

    error 404 ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,46 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'login',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'clima',
+    redirectTo: 'clima',
+    pathMatch: 'full'
+  },
+  {
+    path: 'conversor',
+    redirectTo: 'conversor',
+    pathMatch: 'full'
+  },
+  {
+    path: 'lista-digimon',
+    redirectTo: 'lista-digimon',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'registro',
+    redirectTo: 'registro',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'error',
+    pathMatch: 'full'
+  },
+  {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
--- src/app/error/error.page.ts
+++ src/app/error/error.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ErrorPage implements OnInit {
 
   titulo='404';
-  isNotLogin = true;
+  isNotLogin = false;
 
   constructor() { }
 
